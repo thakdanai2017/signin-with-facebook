8b314749c4c8e3b42eada9abd39e474b3ebc745b
thakdanai chanklom <58160021@go.buu.ac.th>
2017-09-23 11:18:57 +0700

    Add ()Test signin-with-facebook

---
+++ mock.spec.js
@@ -0,0 +1,24 @@
+function Auth(facebook) {
+  this.signInWithFacebook = (phone,password)=>{
+    let value = facebook(phone, password)
+    return value
+  }
+}
+
+test('Mock should return 1',() =>{
+const mockFn =jest.fn()
+  .mockReturnValue(1)
+
+  expect(mockFn()).toBe(1)
+})
+test('Sign in with Facebook valid credential' , () =>{
+
+  let phone = '12345'
+  let password = '54321'
+  const facebook = jest.fn(phone, password)
+    .mockReturnValue({name:"Thakdanai", facebookId: '123456789',email:"58160021@gmail.com"})
+    let app = new Auth(facebook)
+  let obj = app.signInWithFacebook(phone, password)
+  expect(facebook).toBeCalled()
+  expect(obj).toEqual({name: "Thakdanai" , facebookId: '123456789',email:"58160021@gmail.com"})
+})
