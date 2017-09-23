c94c1d31df3b8c9638a8759f6635f617899a18d8
thakdanai chanklom <58160021@go.buu.ac.th>
2017-09-23 12:04:59 +0700

    add function signin with facebook

--- facebook.spec.js
+++ facebook.spec.js
@@ -1,24 +1,43 @@
-function Auth(facebook) {
-  this.signInWithFacebook = (phone,password)=>{
-    let value = facebook(phone, password)
-    return value
-  }
+function Auth(authService) {
+    this.authService = authService
+
+    this.signInWithFacebook = (phone, password) => {
+        return this.authService(phone, password)
+    }
 }
 
 test('Mock with 1 should return 1',() =>{
-const mockFn =jest.fn()
+const mockFn =jest.fn(1)
   .mockReturnValue(1)
 
-  expect(mockFn()).toBe(1)
+  expect(mockFn(1)).toBe(1)
+  expect(mockFn).toBeCalledWith(1)
 })
-test('Sign in with Facebook valid credential' , () =>{
+test('Sign in with Facebook valid credential should pass' , () =>{
+  const returnObject = {
+    name: 'thakdanai',
+    facebookId: '11223344',
+    email: 'thakdanai@gg.com'
+  }
 
-  let phone = '12345'
-  let password = '54321'
-  const facebook = jest.fn(phone, password)
-    .mockReturnValue({name:"Thakdanai", facebookId: '123456789',email:"58160021@gmail.com"})
-    let app = new Auth(facebook)
-  let obj = app.signInWithFacebook(phone, password)
-  expect(facebook).toBeCalled()
-  expect(obj).toEqual({name: "Thakdanai" , facebookId: '123456789',email:"58160021@gmail.com"})
+  const mockFacebook = jest.fn('081777555' , 'asd123')
+    .mockReturnValue(returnObject)
+
+  let app = new Auth(mockFacebook)
+  let value = app.signInWithFacebook('081777555' , 'asd123')
+
+  expect(value).toEqual(returnObject)
+  expect(mockFacebook).toBeCalled()
+  expect(mockFacebook).toBeCalledWith('081777555' , 'asd123')
+  
+  
+  
+  // let phone = '12345'
+  // let password = '54321'
+  // const facebook = jest.fn(phone, password)
+  //   .mockReturnValue({name:"Thakdanai", facebookId: '123456789',email:"58160021@gmail.com"})
+  //   let app = new Auth(facebook)
+  // let obj = app.signInWithFacebook(phone, password)
+  // expect(facebook).toBeCalled()
+  // expect(obj).toEqual({name: "Thakdanai" , facebookId: '123456789',email:"58160021@gmail.com"})
 })
